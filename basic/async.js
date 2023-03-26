48174414ba927e4bb77bd9af9ea98d911c2d9fe5
robinNcode <msmrobin518@gmail.com>
2023-03-26 12:23:28 +0600

    docs added

--- basic/async.js
+++ basic/async.js
@@ -1,5 +1,11 @@
 const processOrder = (customer) => {
     console.log(`Processing order for customer 1`);
+
+    /**
+     * setTimeout is a function that takes a callback function and a time in milliseconds
+     * The callback function is executed after the time specified in milliseconds
+     * The callback function is executed asynchronously
+     */
     setTimeout(() => {
         console.log(`Cooking completed`);
     }, 3000);
