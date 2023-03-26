3b68aeef06840eead9472acc93cbb8f09298d778
robinNcode <msmrobin518@gmail.com>
2023-03-26 13:40:38 +0600

    promise all

---
+++ basic/promise-all.js
@@ -0,0 +1,15 @@
+const promise1 = Promise.resolve('Promise 1 resolved');
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise 2 resolved');
+    }, 2000);
+});
+
+// one after another
+promise1.then(res => console.log(res));
+promise2.then(res => console.log(res));
+
+// all together
+Promise.all([promise1, promise2])
+    .then(res => console.log(res));
