f0ea5f7e350121ee74ac57b626fdcfb9f89ee957
robinNcode <msmrobin518@gmail.com>
2023-03-26 13:11:05 +0600

    callback hell

---
+++ basic/async-controlled.js
@@ -0,0 +1,27 @@
+const takeOrder = (customer, callback) => {
+    console.log(`Take order for ${customer}`);
+    callback(customer);
+}
+
+const processOrder = (customer, callback) => {
+    console.log(`Processing order for ${customer}`);
+
+    setTimeout(() => {
+        console.log(`Cooking completed`);
+        console.log(`Order processed for ${customer}`);
+        callback(customer);
+    }, 3000);
+}
+
+const completeOrder = (customer) => {
+    console.log(`Completed order for ${customer}`);
+}
+
+// Nested callbacks are called callback hell
+takeOrder('Customer 1', (customer) => {
+    processOrder(customer, (customer) => {
+        completeOrder(customer);
+    });
+});
+
+console.log("Hey there!");
